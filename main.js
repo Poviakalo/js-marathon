0b5b3f51e01e5fbdb422a7b7231276d86d148403
Poviakalo <poviasha@gmail.com>
2020-08-20 21:33:31 +0300

    dz-2 task-1

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-alert('VSE good');
+// Завдання 1
+const firstRow = 'мама мыла раму';
+const secondRow = 'собака друг человека';
+let strA1 = '',
+    strA2 = '';
+
+    function getRow(firstRow, secondRow) {
+        for( let i = 0; i < firstRow.length; i++) {            
+            if (firstRow.charAt(i) == 'а') {
+                strA1 += i;
+            }                
+        }
+        for( let i = 0; i < secondRow.length; i++) {            
+            if (secondRow.charAt(i) == 'а') {
+                strA2 += i;
+            }                
+        }
+        return strA1.length > strA2.length ? firstRow : secondRow;        
+    }
+    
+    console.log(getRow(firstRow, secondRow));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
