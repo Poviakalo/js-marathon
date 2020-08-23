450cac6f207da84468e5e51a7d68786256348e4b
Poviakalo <poviasha@gmail.com>
2020-08-23 11:43:51 +0300

    new branch homework-3

--- main.js
+++ main.js
@@ -1,41 +1,62 @@
-// Завдання 1
-const firstRow = 'мама мыла раму';
-const secondRow = 'собака друг человека';
-let strA1 = '',
-    strA2 = '';
+const $btn = document.getElementById('btn-kick');
+const $btnDoubleCick = document.getElementById('btn-double-kick');
 
-    function getRow(firstRow, secondRow) {
-        for( let i = 0; i < firstRow.length; i++) {            
-            if (firstRow.charAt(i) == 'а') {
-                strA1 += i;
-            }                
-        }
-        for( let i = 0; i < secondRow.length; i++) {            
-            if (secondRow.charAt(i) == 'а') {
-                strA2 += i;
-            }                
-        }
-        return strA1.length > strA2.length ? firstRow : secondRow;        
-    }
-    
-    console.log(getRow(firstRow, secondRow));
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    demageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character')
+}
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    demageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy')
+}
 
 
+$btn.addEventListener('click', function() {    
+    changeHP(random(20), character);
+    changeHP(random(20), enemy);
+});
+$btnDoubleCick.addEventListener('click', function() {    
+    changeHP(random(40), character);
+    changeHP(random(40), enemy);
+});
 
+// functions
 
+function init() {
+    console.log('Start Game!');   
+    renderHP(character);
+    renderHP(enemy);
+}
+function renderHP (person) {
+    renderHpLife(person);
+    renderProgressbarHP(person);
+}
+function renderHpLife (person) {         
+    person.elHP.innerText = person.demageHP + '/' + person.defaultHP;    
+}
+function renderProgressbarHP (person) {
+    person.elProgressbar.style.width = person.demageHP + '%';
 
+}
+function changeHP(count, person) {
+    if (person.demageHP < count) {
+        person.demageHP = 0;
+        alert('Бедний ' + person.name + ' проиграл бой!');
+        $btn.disabled = true;
+        $btnDoubleCick.disabled = true;
+    } else {
+        person.demageHP -= count;
+    }    
+    renderHP(person);
+}
+function random (num) {    
+    return Math.ceil(Math.random()*num);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+init();
